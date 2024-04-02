6b294130a2b1c7f8091324839323a297bc7320a1
ranasimr <simranrana10101@gmail.com>
2024-04-02 15:05:56 +0530

    changes to the variations

--- carts/views.py
+++ carts/views.py
@@ -18,15 +18,19 @@ def _cart_id(request):
 # Add a product to the cart
 
 def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product_variation =[]
     if request.method == 'POST':
-     for item in request.POST:
-         key =item
-         value= request.POST[key]
-         try:
-             variation= Variation.objects.get(variation_category_iexact=key,variation_value__iexact=value)
-             print(variation)
-         except:
-             pass    
+        for item in request.POST:
+            key =item
+            value= request.POST[key]
+            
+        
+            try:
+                variation = Variation.objects.get(product=product ,variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass    
 
     product = Product.objects.get(id=product_id)
 
