992a1ef1d773f119f6aa7cfb22f2b4877c142cf5
its-swati <missanu2403@gmail.com>
2024-04-02 20:07:55 +0530

    search changes

--- carts/views.py
+++ carts/views.py
@@ -32,16 +32,21 @@ def add_cart(request, product_id):
             except:
                 pass    
 
-    product = Product.objects.get(id=product_id)
+    # product = Product.objects.get(id=product_id)
 
     
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
+        cart.save()
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation)>0:
+             cart_item.variations.clear()
+             for items in product_variation:
+                cart_item.variations.add(item)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -50,6 +55,11 @@ def add_cart(request, product_id):
             quantity=1,
             cart=cart,
         )
+    if len(product_variation)>0:
+        cart_item.variations.clear()
+        for item in product_variation:
+            cart_item.variations.add(item) 
+        cart_item.save()
     return redirect('cart')
 
 
