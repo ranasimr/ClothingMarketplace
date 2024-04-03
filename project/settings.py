0fd23f515f2fedb282de925020f5150863d024fb
ranasimr <simranrana10101@gmail.com>
2024-04-03 13:28:22 +0530

    regisration changes

--- project/settings.py
+++ project/settings.py
@@ -144,6 +144,13 @@ STATICFILES_DIRS=[
 MEDIA_URL ="/media/"
 MEDIA_ROOT =BASE_DIR /"media"
 
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+}
+
 # EMAIL_HOST ='smtp.gmail.com'
 # EMAIL_PORT =587
 # EMAIL_HOST_USER ='trendify202@gmail.com'
