d358fe3160da508d4d15e7228b11cf97d45d8076
its-swati <missanu2403@gmail.com>
2024-04-04 11:53:53 +0530

    changes

--- carts/views.py
+++ carts/views.py
@@ -4,6 +4,7 @@ from stores.models import Product,Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 # Generate or retrieve cart ID from session
@@ -120,8 +121,13 @@ def remove_cart_item(request, product_id,cart_item_id):
 # View to display the cart
 def cart(request,total=0,quantity=0,cart_items=None):
     try:
-        cart= Cart.objects.get(cart_id=_cart_id(request))
-        cart_items= CartItem.objects.filter(cart=cart,is_active=True)
+        tax=0
+        grand_total=0
+        if request.user.is_authenticated:
+            cart_items= CartItem.objects.filter(user=request.user,is_active=True)
+        else:
+          cart= Cart.objects.get(cart_id=_cart_id(request))
+          cart_items= CartItem.objects.filter(cart=cart,is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
@@ -140,3 +146,31 @@ def cart(request,total=0,quantity=0,cart_items=None):
     }        
 
     return render(request, 'stores/cart.html',context)
+
+
+
+@login_required(login_url='login')
+def checkout(request,total=0,quantity=0,cart_items=None):
+    try:
+        tax=0
+        grand_total=0
+        cart= Cart.objects.get(cart_id=_cart_id(request))
+        cart_items= CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax =(2* total)/100
+        grand_total = total+ tax
+    except ObjectDoesNotExist:
+        pass 
+
+    context ={
+        'total': total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
+
+    }  
+
+    return render(request,'stores/checkout.html',context)
