6702d74314d363724aebd3ae3e8171c470d224a7
its-swati <missanu2403@gmail.com>
2024-04-07 16:07:11 +0530

    review and rating

--- stores/admin.py
+++ stores/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from stores.models import  Product,Variation
+from stores.models import  Product,Variation,ReviewRating
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class VariationAdmin(admin.ModelAdmin):
     list_editable =('is_active',)
     list_filter= ('product', 'variation_category', 'variation_value')
 admin.site.register(Variation,VariationAdmin)
+admin.site.register(ReviewRating)
