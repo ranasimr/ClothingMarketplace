b04ebc6e65555891d60c5f12b210fd38fa00b80d
ranasimr <simranrana10101@gmail.com>
2024-04-08 15:51:43 +0530

    hello

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account ,UserProfile
 from django.contrib.auth.admin import UserAdmin
-
+from django.utils.html import format_html
 class AcoountAdmin(UserAdmin):
     list_display = ('email','first_name','last_name','username','last_login','date_joined','is_active')
     list_display_links =('email','first_name','last_name')
@@ -10,4 +10,11 @@ class AcoountAdmin(UserAdmin):
     filter_horizontal = ()
     list_filter =()
     fieldsets=()
-admin.site.register(Account,AcoountAdmin)
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
+admin.site.register(Account, AcoountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
-from .forms import RegistrationForm  
-from .models import  Account
+from django.shortcuts import render,redirect,get_object_or_404
+from .forms import RegistrationForm ,UserForm,UserProfileForm
+from .models import  Account,UserProfile
+from orders.models import Order
 from django.contrib import messages, auth    
 from django.contrib.auth.decorators import  login_required   
 from django.urls import reverse 
@@ -177,7 +178,15 @@ def activate(request,uidb64,token):
         return redirect('register')
 @login_required(login_url='login')   
 def dashboard(request):
-    return render (request,'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+
+    # userprofile = UserProfile.objects.get(user_id=request.user.id)
+    context = {
+        'orders_count': orders_count,
+        # 'userprofile': userprofile,
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def forgotPassword(request):
@@ -237,4 +246,31 @@ def resetPassword(request):
            
     else:
         return render(request,'accounts/resetPassword.html')
-   
+@login_required(login_url='login')
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'accounts/my_orders.html', context) 
+
+@login_required(login_url='login')
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated.')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile': userprofile,
+    }
+    return render(request, 'accounts/edit_profile.html',context )  
